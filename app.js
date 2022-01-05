54a04ec9546f224c0081352659ac89af433da59e
Jeslin Jayan Varghese <jeslinjaisan@gmail.com>
2022-01-05 14:00:08 +0530

    second

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app =  express();
 
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const path = require('path');
+// const path = require('path');
 
-app.use(express.static('./dist/FrontEnd'));
+// app.use(express.static('./dist/FrontEnd'));
 
 const port = process.env.PORT || 3000;
 
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true })); //middleware portion for adding
 app.use(cors());
 app.use(express.json());
 app.use(express.static('./public'));
-app.use('/uploads', express.static('uploads'))
+app.use('/uploads', express.static('uploads'));
 
 
 // Route for testimonial in home page starts here
@@ -145,13 +145,13 @@ app.use('/api/eventsadmin', eventsadminRouter);
 
 
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist//FrontEnd/index.html'))
- });
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist//FrontEnd/index.html'))
+//  });
 
 
 // port listening to starts here//
 app.listen(port, () => {
-    console.log("Server ready at" + port)
+    console.log("Server ready at" + port);
 });
 // port listening to ends here//
