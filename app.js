e0d89b1426220f06186508b0b3e0a667db7d2341
Jeslin Jayan Varghese <jeslinjaisan@gmail.com>
2022-01-05 23:27:41 +0530

    deletedapi

--- app.js
+++ app.js
@@ -19,48 +19,48 @@ app.use('/uploads', express.static('uploads'));
 
 // Route for testimonial in home page starts here
 const hometestimonialRouter = require('./src/routes/Routes/hometestimonial');
-app.use('/api/hometestimonials',hometestimonialRouter)
+app.use('/hometestimonials',hometestimonialRouter)
 // Route for testimonial for home page ends here
 
 
 // Route for testimonial in home page starts here
 const staffRouter = require('./src/routes/Routes/staff');
-app.use('/api/staff',staffRouter)
+app.use('/staff',staffRouter)
 // Route for testimonial for home page ends here
 
 // Route for patrons starts here
 const patronRouter = require('./src/routes/Routes/patron');
-app.use('/api/patrons',patronRouter)
+app.use('/patrons',patronRouter)
 // Route for patrons ends here
 
 // Route for knowledge partner starts here
 const knowledgepartnerRouter = require('./src/routes/Routes/knowledgepartner');
-app.use('/api/knowledge',knowledgepartnerRouter)
+app.use('/knowledge',knowledgepartnerRouter)
 // Route for knowledge partner starts here
 
 // Route for Industrial partners starts here
 const industrypartnerRouter = require('./src/routes/Routes/industryPartner');
-app.use('/api/industry',industrypartnerRouter)
+app.use('/industry',industrypartnerRouter)
 // Route for Industrial partners ends here
 
 // Partner application form route starts here
 const partnerformRouter = require('./src/routes/Routes/partnerForm');
-app.use('/api/PartnershipApplication', partnerformRouter);
+app.use('/PartnershipApplication', partnerformRouter);
 // Partner application form starts route here
 
 // corporate application form routestarts here
 const corporateformRouter = require('./src/routes/Routes/corporateform');
-app.use('/api/CorporateApplication', corporateformRouter);
+app.use('/CorporateApplication', corporateformRouter);
 // corporate application form route ends here
 
 // academic page route starts here
 const academicRouter = require('./src/routes/Routes/academicMembership');
-app.use('/api/academic', academicRouter);
+app.use('/academic', academicRouter);
 // academic page route  ends here
 
 // Route for events starts here
 const eventsRouter = require('./src/routes/Routes/events');
-app.use('/api/events',eventsRouter)
+app.use('/events',eventsRouter)
 // Route for events ends here
 
 
@@ -68,15 +68,15 @@ app.use('/api/events',eventsRouter)
 
 //Course Card data route
 const Course = require('./src/routes/courses')
-app.use('/api/course',Course)
+app.use('/course',Course)
 
 //Course Registration
 const registration = require('./src/routes/courseRegistration'); //course registration page
-app.use('/api/registration',registration);
+app.use('/registration',registration);
 
 //Course testinomy route
 const testimony = require('./src/routes/courseTestimony');
-app.use('/api/CourseTestimony',testimony)
+app.use('/CourseTestimony',testimony)
 
 
 
@@ -105,44 +105,44 @@ function verifyToken(req, res, next) {
   
 //Admin Login Route
 const Login = require('./src/routes/adminLogin')
-app.use('/api/login',Login)
+app.use('/login',Login)
 
  //Admin logout route
-app.get('/api/logout', (req, res) => {
+app.get('/logout', (req, res) => {
   req.session.destroy();
 }); 
 
 //Admin Signup Route
 const Admin = require('./src/routes/adminSignup')
-app.use('/api/signup',Admin)
+app.use('/signup',Admin)
 
 
 
 
 // patrons admin adding patrons starts here 
 const patronadminRouter = require('./src/routes/Admin/patronadmin');
-app.use('/api/patronadmin', patronadminRouter);
+app.use('/patronadmin', patronadminRouter);
 // patrons admin adding patrons ends here 
 
 // patrons admin adding patrons starts here 
 const industrypartneradminRouter = require('./src/routes/Admin/industrialpartneradmin');
-app.use('/api/industrypartneradmin', industrypartneradminRouter);
+app.use('/industrypartneradmin', industrypartneradminRouter);
 // patrons admin adding patrons ends here 
 
 // patrons admin adding patrons starts here 
 const staffadminRouter = require('./src/routes/Admin/staffadmin');
-app.use('/api/staffadmin', staffadminRouter);
+app.use('/staffadmin', staffadminRouter);
 // patrons admin adding patrons ends here 
 
 // academic admin adding colleges starts here
 const academicadminRouter = require('./src/routes/Admin/academicadmin');
-app.use('/api/academicadmin', academicadminRouter);
+app.use('/academicadmin', academicadminRouter);
 // academic admin adding colleges ends here
 
 
 // events admin adding events starts here
 const eventsadminRouter = require('./src/routes/Admin/eventsadmin');
-app.use('/api/eventsadmin', eventsadminRouter);
+app.use('/eventsadmin', eventsadminRouter);
 // events admin adding events ends here
 
 
